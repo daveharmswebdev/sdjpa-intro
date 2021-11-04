da354680de1905c060a825ee058a4c213c1a45ec
Dave <dave.harms@fortyau.com>
2021-11-04 11:48:14 -0500

    Initial commit

---
+++ src/test/java/dave/greatest/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package dave.greatest.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
