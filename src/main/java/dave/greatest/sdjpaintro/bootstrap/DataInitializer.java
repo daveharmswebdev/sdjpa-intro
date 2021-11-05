f04f01edd61959d6a4459ffc4a80e0597c2c892f
Dave <dave.harms@fortyau.com>
2021-11-05 05:57:33 -0500

    added mysql

--- src/main/java/dave/greatest/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/dave/greatest/sdjpaintro/bootstrap/DataInitializer.java
@@ -3,8 +3,10 @@ package dave.greatest.sdjpaintro.bootstrap;
 import dave.greatest.sdjpaintro.domain.Book;
 import dave.greatest.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile({"local", "default"})
 @Component
 public class DataInitializer implements CommandLineRunner {
 
@@ -16,15 +18,12 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
 
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
-
         Book savedDDD = bookRepository.save(bookDDD);
 
-        System.out.println("Id: " + bookDDD.getId());
-
         Book bookSIA = new Book("Spring in action", "2323232", "Your MOM");
-
         Book savedSIA = bookRepository.save(bookSIA);
 
         bookRepository.findAll()
---
+++ src/test/java/dave/greatest/sdjpaintro/SpringBootJpaTestSlice.java
@@ -0,0 +1,44 @@
+package dave.greatest.sdjpaintro;
+
+import dave.greatest.sdjpaintro.domain.Book;
+import dave.greatest.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Commit;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+@ComponentScan(basePackages = {"dave.greatest.sdjpaintro.bootstrap"})
+public class SpringBootJpaTestSlice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Commit
+    @Order(1)
+    @Test
+    void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+
+        bookRepository.save(new Book("Test title", "123456", "Test publisher"));
+
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(3);
+    }
+}
